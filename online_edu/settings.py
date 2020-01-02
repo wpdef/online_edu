01028c9a5e0639dcb93490d672ad0e35c2ea80c6
WP <wp_def@163.com>
2020-01-02 18:23:05 +0800

    online_edu

--- online_edu/settings.py
+++ online_edu/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'online_edu.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templeats")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,12 +73,16 @@ WSGI_APPLICATION = 'online_edu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'online_edu',
+        'USER': 'online_edu',
+        'PASSWORD':'CjZOt8Dg',
+        'HOST':'47.95.245.204',
+        'PORT':'3306'
+    }
+}
 
 
 # Password validation
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/shanghai'
 
 USE_I18N = True
 
@@ -118,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = [
+os.path.join(BASE_DIR,'static'),
+]
+STATIC_ROOT = [
+os.path.join(BASE_DIR,'static'),
+]
